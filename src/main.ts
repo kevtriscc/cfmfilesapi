8831b4b7f507fad2c2bfd19c39df553cd40cbe22
Kevin Triebnig <kevin.triebnig@auxo-software.com>
2024-02-24 20:58:03 +0100

    feat: secure app

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { JWTStrategy } from '@sap/xssec'
+import { getServices } from '@sap/xsenv'
+import * as passport from 'passport'
+
+const xsuaa = getServices({
+  xsuaa: { name: 'uaa_ExampleCustomCorePluginsMTA' },
+}).xsuaa
+passport.use(new JWTStrategy(xsuaa))
+console.log('xsuaa')
+console.log('xsuaa')
+console.log('xsuaa')
+console.log(xsuaa)
+console.log('xsuaa')
+console.log('xsuaa')
+console.log('xsuaa')
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors({ origin: '*' })
+  app.use(passport.initialize())
+  app.use(passport.authenticate('JWT', { session: false }))
   await app.listen(process.env.PORT || 3000)
 }
 
