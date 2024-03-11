a43cf11ba67d1071e0d1f65c0ce66616d8ad5425
kevtriscc <112697301+kevtriscc@users.noreply.github.com>
2024-03-11 16:23:08 +0100

    Update mfiles.config.ts

--- src/config/mfiles.config.ts
+++ src/config/mfiles.config.ts
@@ -2,6 +2,6 @@ import { registerAs } from '@nestjs/config'
 
 export default registerAs('mfiles', () => ({
   username: process.env.MFILES_USERNAME,
-  password: process.env.MFILES_PASSOWRD,
+  password: process.env.MFILES_PASSWORD,
   vaultGuid: process.env.MFILES_VAULT_GUID,
 }))
