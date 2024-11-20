ba01acbd79052d2863d27ddac07481885f89c9fd
Kevin Triebnig <kevin.triebnig@auxo-software.com>
2024-11-20 12:27:34 +0100

    build: cors from environment variable

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,15 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
-import { JWTStrategy } from '@sap/xssec'
-import { getServices } from '@sap/xsenv'
-import * as passport from 'passport'
-
-// const xsuaa = getServices({
-//   xsuaa: { name: 'uaa_ExampleCustomCorePluginsMTA' },
-// }).xsuaa
-// passport.use(new JWTStrategy(xsuaa))
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import * as process from "node:process";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  app.enableCors({ origin: '*' })
-  // app.use(passport.initialize())
-  // app.use(passport.authenticate('JWT', { session: false }))
-  await app.listen(process.env.PORT || 3000)
+  const app = await NestFactory.create(AppModule);
+  const allowedOrigins = process.env.MFILES_CORS_URL?.split(',') || [];
+  console.log(`allowed origings: ${allowedOrigins}`)
+  app.enableCors({
+    origin: allowedOrigins,
+  })
+  await app.listen(process.env.PORT || 3000);
 }
 
-bootstrap()
+bootstrap();
